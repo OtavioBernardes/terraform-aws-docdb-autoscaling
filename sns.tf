fedad12763da48a4f63d9958ad35520135bfc455
Otavio Bernardes <otavio.bernardes@catho.com>
2023-07-26 17:25:01 -0300

    feat: add topic and subscription to scaledown

--- sns.tf
+++ sns.tf
@@ -1,11 +1,19 @@
-# SNS topic (trigged by CloudWatch)
 resource "aws_sns_topic" "scaleup" {
-  name = var.name
+  name = var.name + "-scaleup"
 }
 
-# Call AWS Lambda when publish messages
+resource "aws_sns_topic" "scaledown" {
+  name = var.name + "-scaledown"
+}
+
 resource "aws_sns_topic_subscription" "scaleup" {
   topic_arn = aws_sns_topic.scaleup.arn
   protocol  = "lambda"
   endpoint  = aws_lambda_function.scaleup.arn
+}
+
+resource "aws_sns_topic_subscription" "scaledown" {
+  topic_arn = aws_sns_topic.scaledown.arn
+  protocol  = "lambda"
+  endpoint  = aws_lambda_function.scaledown.arn
 }
