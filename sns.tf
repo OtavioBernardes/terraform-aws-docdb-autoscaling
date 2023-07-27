9ddaeccae85e34e235935601c3dd8d0fdc4eaf66
Otavio Bernardes <otavio.bernardes@catho.com>
2023-07-27 09:36:50 -0300

    feat: remove unecesseary sns to scaledown

--- sns.tf
+++ sns.tf
@@ -2,18 +2,8 @@ resource "aws_sns_topic" "scaleup" {
   name = "${var.name}-scaleup"
 }
 
-resource "aws_sns_topic" "scaledown" {
-  name = "${var.name}-scaledown"
-}
-
 resource "aws_sns_topic_subscription" "scaleup" {
   topic_arn = aws_sns_topic.scaleup.arn
   protocol  = "lambda"
   endpoint  = aws_lambda_function.scaleup.arn
-}
-
-resource "aws_sns_topic_subscription" "scaledown" {
-  topic_arn = aws_sns_topic.scaledown.arn
-  protocol  = "lambda"
-  endpoint  = aws_lambda_function.scaledown.arn
 }
